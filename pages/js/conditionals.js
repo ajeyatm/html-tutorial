c0d6bff88832b9e031081be5c5e62ee86b9e8a6d
@jeya <ajeya@ormae.com>
2022-09-21 21:27:55 +0530

    updated functions and created conditionals

---
+++ pages/js/conditionals.js
@@ -0,0 +1,11 @@
+let age = 80
+
+if (age < 10) {
+  console.log('Age is less than the 10')
+} else if (age >= 10 && age <= 30) {
+  console.log('Age is between 10 and 30')
+} else if (age >= 31 && age <= 60) {
+  console.log('Age is between 31 and 60')
+} else {
+  console.log('Age is above 60')
+}
--- pages/js/functions.js
+++ pages/js/functions.js
@@ -17,7 +17,7 @@ function hello() {
 }
 //calling the function or function invocation
 
-hello()
+// hello()
 
 //creating function
 
@@ -33,10 +33,10 @@ function someMathOperations(value1, value2, hhh, jsjsj) {
   console.log('final ans- ', sum + mul)
 }
 //calling the function or function invocation
-someMathOperations(x, y)
-someMathOperations(m, n)
-someMathOperations(i, j)
-someMathOperations(100, 200, 400, 5000)
+// someMathOperations(x, y)
+// someMathOperations(m, n)
+// someMathOperations(i, j)
+// someMathOperations(100, 200, 400, 5000)
 
 //firstName, lastName
 let firstName = 'Amit'
@@ -46,4 +46,64 @@ let lastName = 'Sharma'
 function getFullName(firstName, lastName) {
   console.log(firstName.concat(' ', lastName))
 }
-getFullName(lastName, firstName)
+// getFullName(lastName, firstName)
+
+function ageComparison(age) {
+  if (age < 10) {
+    console.log('Age is less than the 10')
+  } else if (age >= 10 && age <= 30) {
+    console.log('Age is between 10 and 30')
+  } else if (age >= 31 && age <= 60) {
+    console.log('Age is between 31 and 60')
+  } else {
+    console.log('Age is above 60')
+  }
+}
+
+// ageComparison(100)
+function areaOfCircle(radius) {
+  console.log(Math.PI * radius)
+}
+function areaOfRectangle(length, width) {
+  console.log(length * width)
+}
+function findArea(geometry, dimensions) {
+  if (geometry === 'circle') {
+    console.log('Area of circle') //PI*r^2
+    console.log(Math.PI * Math.pow(dimensions['radius'], 2)) //PI*r^2
+  } else if (geometry === 'square') {
+    console.log('Area of square') //PI*r^2
+    console.log(dimensions.length * dimensions.length) //l*l
+  } else if (geometry === 'rectangle') {
+    console.log('Area of rectangle') //l*w
+    console.log(dimensions.length * dimensions.width)
+  } else if (geometry === 'triangle') {
+    console.log('Area of triangle') //1/2 * base * height
+    console.log((dimensions.base * dimensions.height) / 2)
+  }
+}
+// findArea('circle', { radius: 10 })
+// findArea('triangle', { height: 10, base: 20 })
+// findArea('rectangle', { length: 10, width: 20 })
+// findArea('square', { length: 10 })
+
+function addTwoNumbers(val1, val2) {
+  return val1 + val2 //40
+}
+
+// let ans = addTwoNumbers(10, 30)
+
+function ageComparisonV2(age) {
+  if (age < 10) {
+    return 'Age is less than the 10'
+  } else if (age >= 10 && age <= 30) {
+    return 'Age is between 10 and 30'
+  } else if (age >= 31 && age <= 60) {
+    return 'Age is between 31 and 60'
+  } else {
+    return 'Age is above 60'
+  }
+}
+
+let ageAns = ageComparisonV2(55)
+console.log(ageAns)
